97e8f4cbf40a4133d907122b98dbbd6839af38e6
Amber Farrington and Zachary Matthewstearn <zachary@>
2016-10-26 13:55:58 -0700

    add full article and link back to index to individual article pages

--- app/components/post-expand.js
+++ app/components/post-expand.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  formattedContent: Ember.computed('post', function() {
+    return Ember.String.htmlSafe(this.get('post.content'));
+  }),
 });
