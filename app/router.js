8956234f69d678df2ed07a93c4f2a51bdb069fb8
Amber Farrington and Zachary Matthewstearn <amber@>
2016-10-26 11:40:59 -0700

    add dynamic routing to individual posts

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('post-expand', {path:'/post/:post_id'});
 });
 
 export default Router;
---
+++ tests/unit/routes/post-expand-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:post-expand', 'Unit | Route | post expand', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
