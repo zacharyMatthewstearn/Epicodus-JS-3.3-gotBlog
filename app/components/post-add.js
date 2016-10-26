bcd2b32eea262262dee35cf77c33fb21b8ca23d8
Amber Farrington and Zachary Matthewstearn <amber+zachary@>
2016-10-26 16:45:51 -0700

    Add functionality for adding new post and attempt to add functionality for saving posts after editing

--- app/components/post-add.js
+++ app/components/post-add.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewPost: false,
+  actions: {
+    savePost() {
+      var params = {
+        title: this.get('title'),
+        subtitle: this.get('subtitle'),
+        author: this.get('author'),
+        content: this.get('content'),
+        snippet: this.get('content').slice(0, 100) + "...",
+        image: this.get('image')
+        // timestamp: moment().format('LLL')
+      };
+      console.log(params);
+
+
+
+      this.set('addNewPost', false);
+      this.sendAction('savePost', params);
+    }
+  }
 });
